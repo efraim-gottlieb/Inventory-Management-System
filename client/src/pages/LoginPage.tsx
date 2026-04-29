909f5c9824224a97fb5144b91f7ec81079a9a6c1
Efraim Gottlieb <efgo58320979@gmail.com>
2026-04-29 15:53:04 +0300

    added LoginPage

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+type LoginData = {
+  username: string;
+  password: string;
+};
+type User = {
+  username: string;
+  password: string;
+};
+type Users = User[];
+
+const users: Users = [
+  {
+    username: "efi",
+    password: "1234",
+  },
+];
+
+function LoginPage() {
+  const [loginData, setLoginData] = useState<LoginData>({
+    username: "",
+    password: "",
+  });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = event.target;
+    setLoginData((prevData: LoginData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+  const login = () => {
+    const user = users.find(
+      (user) =>
+        user.username === loginData.username &&
+        user.password === loginData.password,
+    );
+    alert(user ? "Login successful!" : "Invalid username or password.");
+  };
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <label htmlFor="username">User Name</label>
+      <input type="text" id="username" onChange={handleChange} />
+      <label htmlFor="password">Password</label>
+      <input type="password" id="password" onChange={handleChange} />
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
+
+export default LoginPage;
