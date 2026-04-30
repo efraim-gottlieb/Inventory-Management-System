4a52611cb51dfd319fe976aeaa21b624752b567d
Efraim Gottlieb <efgo58320979@gmail.com>
2026-04-30 11:54:09 +0300

    added dashborad-page tempalte

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,9 +1,26 @@
+import "./dashboard.css";
+
 function Dashboard() {
   return (
-    <div>
-     dashboard 
-    </div>
-  )
+    <>
+      <nav>
+        <h1>Dashboard</h1>
+      </nav>
+      <aside>
+        <h2>Welcome to the Dashboard!</h2>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+      </aside>
+      <main>
+        <h3>Dashboard Content</h3>
+        <p>
+          main
+        </p>
+      </main>
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
