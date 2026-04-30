bc969ecdbb8c2fa85cb71845734df4bd29204a32
Efraim Gottlieb <efgo58320979@gmail.com>
2026-04-30 10:51:36 +0300

    added out of stocl items func

--- server/utils.ts
+++ server/utils.ts
@@ -6,4 +6,9 @@ const calculateInventoryValue = () => {
   return inventoryItems.reduce((total, item) => total + item.quantity * item.price, 0);
 }
 
+const outOfStockItems = () => {
+  return inventoryItems.filter(item => item.quantity === 0);
+}
+
 console.log(calculateInventoryValue())
+console.log(outOfStockItems())
